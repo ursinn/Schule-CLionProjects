830ca6817ba6dde27fa1034084343f92689b0cab
Ursin Filli <ursin.filli@filli-it.ch>
2019-06-18 15:13:25 +0200

    Update Project11

--- M404/Project11/main.cpp
+++ M404/Project11/main.cpp
@@ -3,45 +3,41 @@
 using namespace std;
 
 int main() {
-    int Tage[5];
-    int d;
-    int Summe=0;
-    int Tag;
-    int Ende;
+    int Tage[5], d, Summe=0, Tag, Ende;
 
-    printf("Um zu beginnen, klicke auf 1, um zu beenden auf 2:");
-    scanf("%d", &Ende);
+    cout << "Um zu beginnen, klicke auf 1, um zu beenden auf 2:";
+    cin >> Ende;
     fflush(stdin);
-    printf("\n");
+    cout << endl;
     while (Ende!=2) {
         switch(Ende) {
             case 1:
                 for(d=0;d<5;d++) {
-                    printf("Gebe die Stundenanzahl fuer den %d. Tag an:", d+1);
-                    scanf("%d",&Tage[d]);
+                    cout << "Gebe die Stundenanzahl fuer den " << d+1 << ". Tag an:";
+                    cin >> Tage[d];
                     fflush(stdin);
                     Summe=Summe+Tage[d];
                 }
 
-                printf("Du hast %d Schulstunden in einer Woche\n\n", Summe);
+                cout << "Du hast "<< Summe <<" Schulstunden in einer Woche" << endl << endl;
                 Summe=0;
-                printf("Willst du abfragen, wie viele Stunden du an irgend einem Tag hast,\n dann klicke 3, wenn nicht 4:");
-                scanf("%d", &Tag);
+                cout << "Willst du abfragen, wie viele Stunden du an irgend einem Tag hast,\n dann klicke 3, wenn nicht 4:";
+                cin >> Tag;
                 fflush(stdin);
 
                 if (Tag == 3) {
-                    printf("Von welchem Tag wilst du die Stundenanzahl wissen: ");
-                    scanf("%d", &d);
+                    cout << "Von welchem Tag wilst du die Stundenanzahl wissen: ";
+                    cin >> Tag;
                     fflush(stdin);
-                    printf("An dem Tag %d hast du %d Stunden.\n\n", d, Tage[d - 1]);
-                    printf("Willst du abfragen, wie viele Stunden du an irgend einem Tag hast,\n dann klicke 3, wenn nicht 4:");
-                    scanf("%d", &Tag);
+                    cout << "An dem Tag "<< d <<" hast du " << Tage[d-1] << " Stunden." << endl << endl;
+                    cout << "Willst du abfragen, wie viele Stunden du an irgend einem Tag hast,\n dann klicke 3, wenn nicht 4:";
+                    cin >> Tag;
                     fflush(stdin);
                 }
 
                 if (Tag == 4) {
-                    printf("Um weiter zu machen gebe 1 ein, wenn du nicht weitermachen willst, gebe 2 ein:");
-                    scanf("%d", &Ende);
+                    cout << "Um weiter zu machen gebe 1 ein, wenn du nicht weitermachen willst, gebe 2 ein:";
+                    cin >> Ende;
                     fflush(stdin);
                 }
 
